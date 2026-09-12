ad044f12221cc195a58b7c6e01da727233e88e44
chaurasiyaPratuysh <pratyush222333@gmail.com>
2026-09-12 23:54:50 +0530

    Create GeeksforGeeks_Insertion_Sort.cpp

---
+++ GeeksforGeeks_Insertion_Sort.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    void insertionSort(vector<int>& arr ) {
+        // code here
+        int n=arr.size();
+        for(int i=1; i<n; i++)
+        {
+            int temp=arr[i];
+            int j=i-1;
+            
+            while(j>=0)
+            {
+                if(arr[j]>temp)
+                {
+                    arr[j+1]=arr[j];
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            arr[j+1]=temp;
+        }
+    }
+    
+};
