fa7db7568885ff964b341c5642025f2198ab2fb0
chaurasiyaPratuysh <pratyush222333@gmail.com>
2026-09-21 21:36:35 +0530

    Create 1672_Richest_Customer_Wealth.cpp

---
+++ 1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxvalue=0;
+        int row= accounts.size();
+        int col=accounts[0].size();
+        for(int i=0; i<row; i++)
+        {
+            int sum=0;
+            for(int j=0; j<col; j++)
+            {
+                sum=sum+accounts[i][j];
+                 maxvalue = max(maxvalue, sum);
+            }
+        }
+            return maxvalue;   
+    }
+};
