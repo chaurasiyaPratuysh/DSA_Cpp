c25b66abff0a9fa38f1856086bd61bec01f538ad
chaurasiyaPratuysh <pratyush222333@gmail.com>
2026-09-21 22:00:47 +0530

    Create 1572_Matrix_Diagonal_Sum.cpp

---
+++ 1572_Matrix_Diagonal_Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int ans=0;
+        int n=mat.size();
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==j || i+j == n-1)
+                {
+                   ans=ans+mat[i][j];
+                }
+            }
+        }
+        return ans;
+            
+    }
+};
